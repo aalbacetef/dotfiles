983e6c18b6c04d332d7e66b5f7366e85fe81bea1
Arturo Albacete <aalbacetef@gmail.com>
2022-12-09 01:21:49 +0100

    add kitty

--- kitty/select.sh
+++ kitty/select.sh
@@ -1,34 +1,64 @@
 #!/usr/bin/env bash
 
-if [[ "$1" == ""  ]]; then 
-  echo "" 
+function kitty-parse-args() {
+  if [[ "$1" == ""  ]]; then 
+    echo ""
+    echo ""
+    echo " Available themes: "
+    echo " ------------------"
+    echo ""
+    kitty-ls-themes
+    echo ""
+    echo "Note: there is no need to add the .conf"
+  fi
+}
+
+## For now this function will just call ls and replace with sed, but ideally
+## some cleverer placing of files into columns would be great.
+# 
+#  NEEDS:  $DOTFILES (variable)
+function kitty-ls-themes() {
+  cols=$(tput cols)
+  ls -C -w $cols $DOTFILES/kitty/themes/themes | sed 's/\.conf/     /gm'
+}
+
+
+function kitty-set-theme() {
+  if [[ "$1" == ""  ]]; then 
+    echo ""
+    echo ""
+    echo " Available themes: "
+    echo " ------------------"
+    echo ""
+    kitty-ls-themes
+    echo ""
+    echo "Note: use kitty-set-theme NAME to set the theme"
+    return
+  fi
+
+  name=$(echo "$1" | sed 's/\.conf//')
+  fpath=$DOTFILES/kitty/themes/themes/$name.conf
+  target="$(realpath ~/.config/kitty)/theme.conf"
+
+  if [ ! -f "$fpath" ]; then 
+    echo "theme $name doesn't exist!"
+    return
+  fi
+
+  # delete original link just in case
+  rm -f $target
+
+  # make link
+  echo "linking '$fpath' to => $target"
+  ln -s "$fpath" "$target"
+}
+
+function kitty-help() {
   echo ""
-  echo " Available themes: "
-  echo " ------------------"
+  echo "available kitty commands: "
   echo ""
-  ls $DOTFILES/kitty/themes/themes
+  echo "  * kitty-ls-themes       (display available themes)"
+  echo "  * kitty-set-theme NAME  (set a given theme)"
+  echo "  * kitty-help            (show this help)"
   echo ""
-  echo "Note: there is no need to add the .conf"
-  exit
-fi
-
-
-
-name=$(echo "$1" | sed 's/\.conf//')
-name=$(echo "$name" | sed 's/.*/\u&/')
-fpath=$DOTFILES/kitty/themes/themes/$name.conf
-target="$(realpath ~/.config/kitty)/theme.conf"
-
-if [ ! -f "$fpath" ]; then 
-  echo "them $name doesn't exist!"
-  exit
-fi
-
-# delete original link just in case
-rm -f $target
-
-# make link
-echo "linking '$fpath' to => $target"
-
-ln -s "$fpath" "$target"
-
+}
