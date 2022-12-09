60023b9056fd56990a94bf1d94a97165e145a118
Arturo Albacete <aalbacetef@gmail.com>
2022-12-09 01:31:51 +0100

    mkd: handle empty path

--- bash/extension.sh
+++ bash/extension.sh
@@ -3,6 +3,10 @@
 function mkd() {
   local arg="$1"
   local path="$2"
+  if [[ "$arg" == "" ]]; then 
+    echo "no path specified"
+    return 1
+  fi 
 
   if [[ "$path" == "" ]]; then
     path="$1"
