055b89cad5675f799eff21f4ca3cdf990c58472b
Arturo Albacete <aalbacetef@gmail.com>
2023-01-11 08:44:24 +0100

    update bash settings

--- bash/extension.sh
+++ bash/extension.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
+
 function mkd() {
-  local arg="$1"
-  local path="$2"
-  if [[ "$arg" == "" ]]; then 
-    echo "no path specified"
-    return 1
-  fi 
+  local path="$1"
 
-  if [[ "$path" == "" ]]; then
-    path="$1"
-  fi
-
-  mkdir "$arg" "$path" && cd "$path"
+  mkdir -p "$path" && cd "$path"
 }
