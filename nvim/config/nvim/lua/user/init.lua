a77b8f77a3dab24fe9b13ae0ec381e3cbed95d9b
Arturo Albacete <aalbacetef@gmail.com>
2023-10-11 20:50:48 +0200

    add nvim config

---
+++ nvim/config/nvim/lua/user/init.lua
@@ -0,0 +1,9 @@
+local settings = {}
+
+settings["colorscheme"] = "catppuccin"
+
+-- lsp_signature -- 
+-- local cfg = {}  -- add your config here
+-- require "lsp_signature".setup(cfg)
+
+return settings
