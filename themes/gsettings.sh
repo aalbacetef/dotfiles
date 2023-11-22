98255e22ecef072acd091fb777ea0d256af93f51
Arturo Albacete <aalbacetef@gmail.com>
2023-11-22 17:14:15 +0100

    added themes gsettings

---
+++ themes/gsettings.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+gsettings set org.gnome.Terminal.Legacy.Settings theme-variant system
+gsettings set org.gnome.desktop.interface cursor-theme Pop
+gsettings set org.gnome.desktop.interface gtk-key-theme Default
+gsettings set org.gnome.desktop.interface gtk-theme dracula-arturo
+gsettings set org.gnome.desktop.interface icon-theme Tela
+gsettings set org.gnome.desktop.sound theme-name Pop
+gsettings set org.gnome.desktop.wm.preferences theme Pop
+gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme false
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-running-dots false
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-color #ffffff
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-width 0
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-color #ffffff
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
+gsettings set org.gnome.shell.extensions.user-theme name dracula-arturo
