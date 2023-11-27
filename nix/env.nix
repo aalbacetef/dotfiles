a862db980269846e9cbd73eeefa1ca7864e70532
Arturo Albacete <aalbacetef@gmail.com>
2023-11-27 17:56:09 +0100

    add nix default programs

---
+++ nix/env.nix
@@ -0,0 +1,18 @@
+with import <nixpkgs> {}; [
+  coreutils-full
+  fd
+  gdb
+  jq
+  nasm
+  neovim
+  nodejs
+  obsidian
+  python3
+  ripgrep
+  ruby_3_1
+  shellcheck
+  socat
+  tmux
+  tree
+  universal-ctags
+]
