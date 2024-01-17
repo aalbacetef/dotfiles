61648575e16e3e1789c3e9c592077d6828c6446e
Arturo Albacete <aalbacetef@gmail.com>
2024-01-18 00:37:42 +0100

    add comments and formatting to do extension

--- bash/helpers/doctl.sh
+++ bash/helpers/doctl.sh
@@ -1,5 +1,16 @@
+#!/usr/bin/env bash 
 
 
+##########################################
+# Get the ID of the digitalocean firewall 
+# corresponding to "$NAME".
+#
+# Arguments:
+#  - Name of the firewall 
+#
+# Outputs:
+#  - ID of the firewall
+##########################################
 doctl::get_firewall_id() {
   local name="$1"
   if [[ "$name" == "" ]]; then
@@ -7,5 +18,7 @@ doctl::get_firewall_id() {
     return 1
   fi
 
-  doctl compute firewall list --format 'ID,Name'| grep -E "\s$name$" | cut -d' ' -f 1
+  doctl compute firewall list --format 'ID,Name' | grep -E "\s$name$" | cut -d' ' -f 1
 }
+
+
