02b909d5c9ce0bc95036ca0bfcbdb63151fb383f
Arturo Albacete <aalbacetef@gmail.com>
2024-04-28 00:01:07 +0300

    darwin[defaults]: don't create DS_Store on SMB and USB

--- darwin/defaults.sh
+++ darwin/defaults.sh
@@ -2,3 +2,10 @@
 
 # disable mac os' weird mouse handling
 defaults write -g com.apple.mouse.scaling 1.0
+
+# disable DS_Store on SMB shares
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true 
+
+# disable DS_Store on USBs
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true 
+
