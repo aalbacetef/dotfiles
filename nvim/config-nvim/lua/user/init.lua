ca77b3b335398b7cb80d6c447df9e3051b2be1f9
Arturo Albacete <aalbacetef@gmail.com>
2024-06-24 17:57:03 +0200

    nvim: added user

---
+++ nvim/config-nvim/lua/user/init.lua
@@ -0,0 +1,24 @@
+local settings = {}
+
+settings["colorscheme"] = "catppuccin"
+
+settings["lsp"] = {
+  formatting = {
+    format_on_save = true,
+  },
+}
+
+settings["options"] = function(local_vim)
+  local_vim.opt.wrap = true 
+  return local_vim
+end
+
+
+vim.filetype.add({
+  extension = {
+    mdx = 'markdown.mdx'
+  }
+})
+
+
+return settings
