3e25ece6da2d53f12b18be7acd87e914527740ac
Arturo Albacete <aalbacetef@gmail.com>
2024-06-28 11:44:50 +0200

    bash: add alias for git status

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,6 +2,10 @@
 
 source "$DOTFILES/bash/helpers/platform.sh"
 
+
+# NOTE: this one might shadow ghostscript
+alias gs='git status'
+
 alias sizeof='du -h -d 0'
 alias ls='ls --color'
 alias l='ls'
