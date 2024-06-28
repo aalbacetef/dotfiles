6c84547cfad54b7ddf4eb363a3a5db9d7b98adfa
Arturo Albacete <aalbacetef@gmail.com>
2024-06-28 12:45:51 +0200

    git: ensure pull rebase is the default, add excludes file

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -1,7 +1,6 @@
-
 #!/usr/bin/env bash
 
-## use my ssh aliases for git repors
+## use my ssh aliases for git repos
 git config --global url."gitlab:".insteadOf "https://gitlab.com/"
 git config --global url."github:".insteadOf "https://github.com"
 
@@ -10,3 +9,12 @@ git config --global init.defaultbranch master
 
 # use no fast forward as default merge strategy
 git config --global --add merge.ff false
+
+# automatically rebase when pulling
+git config --global --add pull.rebase preserve
+
+# set excludes file
+current_dir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+excludes_file="$current_dir/git-excludes-file"
+
+git config --global --add core.excludesfile "$excludes_file"
