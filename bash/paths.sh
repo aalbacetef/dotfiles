56af2f461cc27cce3aad905ed33871d344c22c78
Arturo Albacete <aalbacetef@gmail.com>
2024-07-02 11:37:41 +0200

    bash: add local bin should only run for darwin

--- bash/paths.sh
+++ bash/paths.sh
@@ -23,6 +23,8 @@ if test -n "$docker_host"; then
   export DOCKER_HOST="$docker_host"
 fi 
 
+
+
 shadowed_docker_host=$(env::get "$envrc" SHADOWED_DOCKER_HOST)
 if test -n "$shadowed_docker_host"; then 
   export SHADOWED_DOCKER_HOST="$shadowed_docker_host"
@@ -30,7 +32,6 @@ fi
 
 if platform::is_linux; then 
   xdg_data_dirs="$(env::get "$envrc" XDG_DATA_DIRS)"
-  add_local_bin=$(env::get "$envrc" ADD_LOCAL_BIN)
 
   if test -n "$add_local_bin"; then 
     export PATH="/usr/local/bin:$PATH"
@@ -39,4 +40,11 @@ if platform::is_linux; then
   if test -n "$xdg_data_dirs"; then 
     export XDG_DATA_DIRS="$xdg_data_dirs:$XDG_DATA_DIRS"
   fi 
+fi
+
+if platform::is_darwin; then
+  add_local_bin=$(env::get "$envrc" ADD_LOCAL_BIN)
+  if test -n "$add_local_bin"; then 
+    export PATH="/usr/local/bin:$PATH"
+  fi
 fi
