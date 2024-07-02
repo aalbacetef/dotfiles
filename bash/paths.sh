a336f7d412bd980ab6cd2f0456c6ae9e5ff20ec1
Arturo Albacete <aalbacetef@gmail.com>
2024-07-02 03:07:33 +0200

    bash: cleanup paths.sh to use test instead of [[ and ensure gopath is added to path

--- bash/paths.sh
+++ bash/paths.sh
@@ -5,30 +5,33 @@ source "$DOTFILES/bash/helpers/platform.sh"
 
 envrc="$DOTFILES/bash/env.rc"
 
-export GO_BIN="$(go env bin)"
+if GO_BIN="$(go env GOPATH)/bin"; then
+  export PATH="$GO_BIN:$PATH"
+fi
+
 export NVM_DIR="$HOME/.nvm"
 
 export KITTY_CONFIG_DIRECTORY="$DOTFILES/kitty"
 
 vagrant_home=$(env::get "$envrc" VAGRANT_HOME)
-if [[ "$vagrant_home" != "" ]]; then 
+if test -n "$vagrant_home"; then 
   export VAGRANT_HOME="$vagrant_home"
 fi
 
 docker_host=$(env::get "$envrc" DOCKER_HOST)
-if [[ "$docker_host" != "" ]]; then 
+if test -n "$docker_host"; then 
   export DOCKER_HOST="$docker_host"
 fi 
 
 shadowed_docker_host=$(env::get "$envrc" SHADOWED_DOCKER_HOST)
-if [[ "$shadowed_docker_host" != "" ]]; then 
+if test -n "$shadowed_docker_host"; then 
   export SHADOWED_DOCKER_HOST="$shadowed_docker_host"
 fi
 
-if [[ "$(platform::is_linux)"  == "1" ]]; then 
+if platform::is_linux; then 
   xdg_data_dirs="$(env::get "$envrc" XDG_DATA_DIRS)"
 
-  if [[ "$xdg_data_dirs" != "" ]]; then 
+  if test -n "$xdg_data_dirs"; then 
     export XDG_DATA_DIRS="$xdg_data_dirs:$XDG_DATA_DIRS"
   fi 
 fi
