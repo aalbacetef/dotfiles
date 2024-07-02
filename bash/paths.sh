5e355402134f602649349d6eaa6492d34986b77b
Arturo Albacete <aalbacetef@gmail.com>
2024-07-02 11:54:00 +0200

    bash: add paths only if they are not already present in the PATH var

--- bash/paths.sh
+++ bash/paths.sh
@@ -6,7 +6,16 @@ source "$DOTFILES/bash/helpers/platform.sh"
 envrc="$DOTFILES/bash/env.rc"
 
 if GO_BIN="$(go env GOPATH)/bin"; then
-  export PATH="$GO_BIN:$PATH"
+  if ! (echo "$PATH" | grep -q "$GO_BIN") ; then 
+    export PATH="$GO_BIN:$PATH"
+  fi
+fi
+
+add_local_bin=$(env::get "$envrc" ADD_LOCAL_BIN)
+if test -n "$add_local_bin"; then 
+  if ! (echo "$PATH" | grep -q "$add_local_bin"); then 
+    export PATH="$add_local_bin:$PATH"
+  fi
 fi
 
 export NVM_DIR="$HOME/.nvm"
@@ -23,8 +32,6 @@ if test -n "$docker_host"; then
   export DOCKER_HOST="$docker_host"
 fi 
 
-
-
 shadowed_docker_host=$(env::get "$envrc" SHADOWED_DOCKER_HOST)
 if test -n "$shadowed_docker_host"; then 
   export SHADOWED_DOCKER_HOST="$shadowed_docker_host"
@@ -33,18 +40,7 @@ fi
 if platform::is_linux; then 
   xdg_data_dirs="$(env::get "$envrc" XDG_DATA_DIRS)"
 
-  if test -n "$add_local_bin"; then 
-    export PATH="/usr/local/bin:$PATH"
-  fi
-
   if test -n "$xdg_data_dirs"; then 
     export XDG_DATA_DIRS="$xdg_data_dirs:$XDG_DATA_DIRS"
   fi 
-fi
-
-if platform::is_darwin; then
-  add_local_bin=$(env::get "$envrc" ADD_LOCAL_BIN)
-  if test -n "$add_local_bin"; then 
-    export PATH="/usr/local/bin:$PATH"
-  fi
 fi
