57951ce500e8d9ba2ee191d4799260b28a924e6f
Arturo Albacete <aalbacetef@gmail.com>
2024-09-12 11:59:38 +0200

    nix: restructure flake to pass pkgs as a param to lists, which plays nicely with nested packages; add roc and reason

--- nix/flake.nix
+++ nix/flake.nix
@@ -9,140 +9,142 @@
 
   outputs = { self, nixpkgs, roc, ... }:
     let
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+      };
+
+      pkgsLinux = pkgsFor "x86_64-linux";
+      pkgsDarwin = pkgsFor "x86_64-darwin";
+
+      essentials = sysPkgs: with sysPkgs; [
+        bash-completion
+        cmake
+        curl
+        coreutils-full
+        fd
+        fzf
+        gdb
+        git
+        gnumake
+        gnuplot
+        golangci-lint
+        go-task
+        go-toml
+        graphviz
+        grpc
+        imagemagick
+        libssh2
+        nasm
+        nerdfonts
+        openssl_3_2
+        podman
+        qemu
+        ripgrep
+        shellcheck
+        socat
+        tree
+        universal-ctags
+        wasmtime
+        wget
       ];
 
-      essentials = [
-        "bash-completion"
-        "cmake"
-        "curl"
-        "coreutils-full"
-        "fd"
-        "fzf"
-        "gdb"
-        "git"
-        "gnumake"
-        "gnuplot"
-        "golangci-lint"
-        "go-task"
-        "go-toml"
-        "graphviz"
-        "grpc"
-        "imagemagick"
-        "libssh2"
-        "nasm"
-        "nerdfonts"
-        "openssl_3_2"
-        "podman"
-        "qemu"
-        "ripgrep"
-        "shellcheck"
-        "socat"
-        "tree"
-        "universal-ctags"
-        "wasmtime"
-        "wget"
+      apps = sysPkgs: with sysPkgs; [
+        ansible
+        ansible-lint
+        asciidoctor
+        bun
+        devenv
+        doctl
+        emacs
+        ffmpeg
+        fish
+        gh
+        glab
+        helix
+        htop
+        http-server
+        httpie
+        jq
+        kitty
+        macchina
+        minio-client
+        netcat
+        nmap
+        nmap-formatter
+        neovim
+        obsidian
+        terraform
+        thc-hydra
+        tmux
+        yarn
+        yq
       ];
 
-      apps = [
-        "ansible"
-        "ansible-lint"
-        "asciidoctor"
-        "bun"
-        "devenv"
-        "doctl"
-        "emacs"
-        "ffmpeg"
-        "fish"
-        "gh"
-        "glab"
-        "helix"
-        "htop"
-        "http-server"
-        "httpie"
-        "jq"
-        "kitty"
-        "macchina"
-        "minio-client"
-        "netcat"
-        "nmap"
-        "nmap-formatter"
-        "neovim"
-        "obsidian"
-        "terraform"
-        "thc-hydra"
-        "tmux"
-        "yarn"
-        "yq"
+      langs = sysPkgs: with sysPkgs; [
+        elixir
+        go
+        lua
+        luarocks
+        maxima
+        nodejs
+        ocamlPackages.reason
+        poetry
+        python312
+        racket
+        ruby
+        rustup
+        sbcl
+        scala
+        solc
+        zig
       ];
 
-      langs = [
-        "elixir"
-        "go"
-        "lua"
-        "luarocks"
-        "maxima"
-        "nodejs"
-        "poetry"
-        "python312"
-        "racket"
-        "ruby"
-        "rustup"
-        "sbcl"
-        "scala"
-        "solc"
-        "zig"
-      ];
+      commonPackages = sysPkgs: apps sysPkgs ++ langs sysPkgs ++ essentials sysPkgs;
 
-      commonPackages = apps ++ langs ++ essentials;
+      linuxPkgs = with pkgsLinux; [
+        autotools-language-server
+        calibre
+        checksec
+        chromium
+        ghdl
+        koreader
+        ngspice
+        octaveFull
+        qucs-s
+        remmina
+        shfmt
+        signal-desktop
+        vagrant
+      ] ++ commonPackages pkgsLinux;
 
-      linuxPkgs = [
-        "autotools-language-server"
-        "calibre"
-        "checksec"
-        "chromium"
-        "ghdl"
-        "koreader"
-        "ngspice"
-        "octaveFull"
-        "qucs-s"
-        "remmina"
-        "shfmt"
-        "signal-desktop"
-        "vagrant"
-      ] ++ commonPackages;
-
-      darwinPkgs = [
-        "alacritty"
-        "rectangle"
-        "skhd"
-        "texliveMedium"
-        "texstudio"
-      ] ++ commonPackages;
-
+      darwinPkgs = with pkgsDarwin; [
+        alacritty
+        rectangle
+        skhd
+        texliveMedium
+        texstudio
+      ] ++ commonPackages pkgsDarwin;
 
     in
     {
       packages.x86_64-linux.default =
         let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          pkgs = pkgsLinux;
           rocPkgs = roc.packages.x86_64-linux;
         in
         pkgs.buildEnv {
           name = "user-linux-packages";
-          paths = (map (p: pkgs.${p}) linuxPkgs) ++ [ rocPkgs.cli ];
+          paths = linuxPkgs ++ [ rocPkgs.cli ];
         };
 
       packages.x86_64-darwin.default =
         let
-          pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+          pkgs = pkgsDarwin;
           rocPkgs = roc.packages.x86_64-darwin;
         in
         pkgs.buildEnv {
           name = "user-darwin-packages";
-          paths = (map (p: pkgs.${p}) darwinPkgs) ++ [ rocPkgs.cli ];
+          paths = darwinPkgs ++ [ rocPkgs.cli ];
         };
     };
 }
