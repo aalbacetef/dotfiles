68213d15109a76e48d02f5d8734a71eb169d841b
Arturo Albacete <aalbacetef@gmail.com>
2025-02-08 15:01:18 +0100

    git: fix typo in excludes file; unset config values before setting them; add an ignore for the .DS_Store file when working on mac

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -5,19 +5,24 @@ git config --global url."gitlab:".insteadOf "https://gitlab.com/"
 git config --global url."github:".insteadOf "https://github.com"
 
 # set default branch name
+git config --global --unset-all init.defaultbranch
 git config --global init.defaultbranch master
 
 # use no fast forward as default merge strategy
-git config --global --add merge.ff false
+git config --global --unset-all merge.ff
+git config --global merge.ff false
 
 # automatically rebase when pulling
-git config --global --add pull.rebase true
+git config --global --unset-all pull.rebase
+git config --global pull.rebase true
 
 # set excludes file
 current_dir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-excludes_file="$current_dir/git-excludes-file"
+excludes_file="$current_dir/git-excludesfile"
 
-git config --global --add core.excludesfile "$excludes_file"
+git config --global --unset-all core.excludesfile
+git config --global core.excludesfile "$excludes_file"
 
 # set meld as the merge tool
-git config --global --add merge.tool meld
+git config --global --unset-all merge.tool
+git config --global merge.tool meld
