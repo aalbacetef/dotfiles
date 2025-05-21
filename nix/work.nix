303e877fa60d597ce851fef8154df6368165e3ea
Arturo Albacete <aalbacetef@gmail.com>
2025-05-21 18:21:50 +0200

    nix: disable go package installation on Linux as this requires removing the sandbox (and I'm not sure if I want to do that)

--- nix/work.nix
+++ nix/work.nix
@@ -1,44 +1,51 @@
-{ sysPkgs }: with sysPkgs; [
-  go-ethereum
-  golangci-lint
-  google-cloud-sdk
-  go-task
-  kubectl
-  kustomize
-  krew
-  sqlc
+  { sysPkgs }:
+with sysPkgs;
 
-  ## additional
-  _1password-cli
-  google-cloud-sql-proxy
-  foundry
+  let 
+    isLinux = builtins.match "x86_64-linux" system != null;
+    workPkgs = [
+      go-ethereum
+      golangci-lint
+      google-cloud-sdk
+      go-task
+      kubectl
+      kustomize
+      krew
+      sqlc
 
-  (stdenv.mkDerivation {
-    name = "custom-go-pkgs";
-    src = pkgs.emptyDirectory;
-    buildInputs = [ 
-      go_1_24  
-      git
+      ## additional
+      _1password-cli
+      google-cloud-sql-proxy
+      foundry
     ];
-    nativeBuildInputs = [ go_1_24 ];
+
+    goPkgs = if isLinux then [] else [(stdenv.mkDerivation {
+      name = "custom-go-pkgs";
+      src = pkgs.emptyDirectory;
+      buildInputs = [ 
+        go_1_24  
+        git
+      ];
+      nativeBuildInputs = [ go_1_24 ];
 
 
-    postInstall = ''
-      export GOCACHE=$TMPDIR/go-cache
-      export GOPATH=$TMPDIR/go-path
-      export GOBIN=$out/bin
+      postInstall = ''
+        export GOCACHE=$TMPDIR/go-cache
+        export GOPATH=$TMPDIR/go-path
+        export GOBIN=$out/bin
 
-      echo "Installing additional go packages..."
+        echo "Installing additional go packages..."
 
-      ${go_1_24}/bin/go install github.com/go-delve/delve/cmd/dlv@v1.24.2
-      ${go_1_24}/bin/go install mvdan.cc/gofumpt@v0.8.0
-      ${go_1_24}/bin/go install github.com/segmentio/golines@v0.12.2
-      ${go_1_24}/bin/go install gotest.tools/gotestsum@v1.12.2
-      ${go_1_24}/bin/go install github.com/golang/mock/mockgen@v1.6.0
-      ${go_1_24}/bin/go install github.com/gohugoio/hugo@v0.147.4
-      ${go_1_24}/bin/go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@v4.18.3
+        ${go_1_24}/bin/go install github.com/go-delve/delve/cmd/dlv@v1.24.2
+        ${go_1_24}/bin/go install mvdan.cc/gofumpt@v0.8.0
+        ${go_1_24}/bin/go install github.com/segmentio/golines@v0.12.2
+        ${go_1_24}/bin/go install gotest.tools/gotestsum@v1.12.2
+        ${go_1_24}/bin/go install github.com/golang/mock/mockgen@v1.6.0
+        ${go_1_24}/bin/go install -tags 'expanded' github.com/gohugoio/hugo@v0.130.0
+        ${go_1_24}/bin/go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@v4.18.3
 
-      echo "Done"
-    '';
-  })
-]
+        echo "Done"
+      '';
+    })];
+  in
+    workPkgs ++ goPkgs
