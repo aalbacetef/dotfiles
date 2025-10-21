4971f91baabb66c4c47700771d1a114007311dd3
Arturo Albacete <aalbacetef@gmail.com>
2025-10-21 21:01:49 +0200

    bash: add aliases for nix and git ops, update personal-ssh-ip method for do (previous service was returning ipv6)

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,8 +1,15 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
+
+## git
 
 # NOTE: this one might shadow ghostscript
 alias gs='git status'
+alias glast='git log -n 1 -p -v'
 
+## general
 alias sizeof='du -h -d 0'
 alias ls='ls --color'
 alias l='ls'
+
+## nix
+alias nix-search='nix search nixpkgs'
--- bash/extension.sh
+++ bash/extension.sh
@@ -14,7 +14,7 @@ function mkd() {
 
 function personal-ssh-ip() {
   local my_ip
-  my_ip="$(curl -s ifconfig.me)"
+  my_ip="$(curl -s api.ipify.org)"
 
   local firewall_id
   local firewall_name="personal-ssh"
@@ -22,39 +22,37 @@ function personal-ssh-ip() {
   local rule_to_add="protocol:tcp,ports:22,address:$my_ip"
 
   firewall_id=$(doctl::get_firewall_id "$firewall_name")
-  if [[ "$firewall_id" == "" ]]; then 
+  if [[ "$firewall_id" == "" ]]; then
     echo "No firewall found"
     return 1
   fi
-  
+
   echo "found firewall with id: $firewall_id"
 
   rules_to_delete=$(doctl::get_rules "$firewall_id")
-  if [[ "$rules_to_delete" != "" ]]; then 
+  if [[ "$rules_to_delete" != "" ]]; then
     echo "removing previous inbound rule: $rules_to_delete"
     doctl::remove_rules "$firewall_id" "$rules_to_delete"
   fi
-  
 
   echo "adding inbound rule: $rule_to_add"
   doctl::add_rule "$firewall_id" "$rule_to_add"
 }
-
 
 function remove-personal-ssh-ip() {
   local firewall_id
   local rules_to_delete
 
   firewall_id=$(doctl::get_firewall_id "personal-ssh")
-  if test -z "$firewall_id"; then 
+  if test -z "$firewall_id"; then
     echo "No firewall found"
     return 1
   fi
-  
+
   echo "found firewall with id: $firewall_id"
 
   rules_to_delete=$(doctl::get_rules "$firewall_id")
-  if test -z "$rules_to_delete"; then 
+  if test -z "$rules_to_delete"; then
     echo "no rules to remove"
     return 0
   fi
@@ -73,7 +71,6 @@ ddo-launch() {
 
   local pfx
   local rootdir
-
 
   pfx=$(realpath ~/.steam/debian-installation/steamapps/compatdata/206480/pfx)
   rootdir="$(realpath ~/.steam/debian-installation/steamapps/common/Dungeons\ and\ Dragons\ Online)"
@@ -100,10 +97,9 @@ cloud-logout() {
   echo "[cloud-logout] ...done"
 }
 
-
 ##############################
 #
-# Runs a nix profile upgrade 
+# Runs a nix profile upgrade
 #
 ##############################
 nix-up() {
@@ -113,7 +109,7 @@ nix-up() {
   echo "[nix-up] starting nix profile upgrade..."
   echo "[nix-up]   - name: $profile_name"
   echo "[nix-up]   - flags: $flags"
-  
+
   nix profile upgrade "$profile_name" $flags
 
   echo "[nix-up] ...done"
@@ -121,13 +117,41 @@ nix-up() {
 
 #######################################
 #
-# Will watch the current directory  
-# and run the command passed in 
+# Will watch the current directory
+# and run the command passed in
 #
-# Example: 
+# Example:
 #   watch-n-run go run ./cmd/server/
 #
 #######################################
 watch-n-run() {
   "$DOTFILES/scripts/watch-n-run.fish" "$@"
+}
+
+###########################################
+#
+# Will lookup the package info and output
+# it in JSON format.
+#
+###########################################
+np-info() {
+  local pkgname="$1"
+  local additional="$2"
+
+  if test -z "$pkgname"; then
+    echo "package must be specified"
+    return 1
+  fi
+
+  if test "$additional" = "--short"; then
+    nix-env -qa --description "$pkgname" 2>/dev/null | fold -w 80
+    return 0
+  fi
+
+  if test "$additional" = "--less"; then
+    nix-env -qa --meta --json "$pkgname" 2>/dev/null | jq -C | less -r
+    return 0
+  fi
+
+  nix-env -qa --meta --json "$pkgname" 2>/dev/null | jq -C
 }
