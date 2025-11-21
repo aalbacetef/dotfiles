d4edb52b69a568c3691c045d7b125b5a796e3522
Arturo Albacete <aalbacetef@gmail.com>
2025-11-21 05:37:26 +0100

    bash: add helpers for wine and for asking LLM

--- bash/extension.sh
+++ bash/extension.sh
@@ -319,20 +319,128 @@ bw-mk-note() {
 
 ###############################################
 #
-# Helper for setting up a pfx when installing
-# games with wine.
+# Helper for installing games with wine.
 #
-##################$$$$$$$$$$$$$$$$$$$$#########
+###############################################
 wine-install() {
   local current_dir
   local pfx
   local exe_bin
+  local game
 
-  exe_bin="$1"
+  exe_bin="$(realpath "$1")"
   current_dir="$(realpath "$(pwd)")"
+  game="$current_dir/game"
   pfx="$current_dir/pfx"
 
-  mkdir -p "$pfx"
+  wine-pfx-setup
 
-  env WINEPREFIX="$pfx" wine "$exe_bin"
+  ({
+    export WINEPREFIX="$pfx"
+
+    cd "$(dirname "$exe_bin")"
+
+    base_name="$(basename "$exe_bin")"
+    echo "running: $base_name"
+    wine "$(basename "$exe_bin")"
+  })
+}
+
+###############################################
+#
+# Helper for setting up a pfx when installing
+# games with wine.
+#
+###############################################
+wine-pfx-setup() {
+  local current_dir
+  local pfx
+  local game
+
+  current_dir="$(realpath "$(pwd)")"
+  game="$current_dir/game"
+  pfx="$current_dir/pfx"
+
+  export WINEPREFIX="$pfx"
+  winecfg /v win7
+
+  mkdir -p "$pfx"
+  mkdir -p "$game"
+
+  cat <<EOF >"$current_dir/files.reg"
+Windows Registry Editor Version 5.00
+
+[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders]
+"Desktop"="Z:{{ .GAME }}/game/Desktop"
+"Personal"="Z:{{ .GAME }}/game/Documents"
+"My Pictures"="Z:{{ .GAME }}/game/Pictures"
+"My Music"="Z:{{ .GAME }}/game/Music"
+"My Video"="Z:{{ .GAME }}/game/Videos"
+"My Templates"="Z:{{ .GAME }}/game/Templates"
+"{7D83EE9B-3736-4BAB-956B-4D1DC0E1F7CC}"="Z:{{ .GAME }}/game/Downloads"
+
+[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders]
+"Desktop"="%USERPROFILE%/Desktop"
+"Personal"="%USERPROFILE%/Documents"
+"My Pictures"="%USERPROFILE%/Pictures"
+"My Music"="%USERPROFILE%/Music"
+"My Video"="%USERPROFILE%/Videos"
+"{7D83EE9B-3736-4BAB-956B-4D1DC0E1F7CC}"="%USERPROFILE%/Downloads"
+EOF
+
+  sed -i "s@"'{{ .GAME }}'"@$current_dir@gm" "$current_dir/files.reg"
+  sed -i 's@/@\\\\@gm' "$current_dir/files.reg"
+
+  local targets="Desktop Documents Pictures Music Videos Downloads Templates"
+
+  local src_dir="$pfx/drive_c/users/arturo"
+  local target_dir="$current_dir/game"
+
+  for d in $targets; do
+    if test -L "$src_dir/$d"; then
+      rm "$src_dir/$d"
+    fi
+
+    if test -d "$src_dir/$d"; then
+      rm -rf "$src_dir/$d"
+    fi
+
+    mkdir -p "$target_dir/$d"
+    ln -s "$target_dir/$d" "$src_dir/$d"
+  done
+
+  wine regedit "$current_dir/files.reg"
+  rm "$current_dir/files.reg"
+}
+
+############################
+#
+# Ask a message via aider
+#
+############################
+ask() {
+  if test -z "$1"; then
+    echo "please provider a message: ask MESSAGE"
+    return 1
+  fi
+
+  local conversation_folder="$(realpath ~/.cache/conversation)"
+  local input_history_file="$conversation_folder/aider.input.history"
+  local chat_history_file="$conversation_folder/aider.chat.history"
+  local llm_history_file="$conversation_folder/aider.llm.history"
+
+  if ! test -d "$conversation_folder"; then
+    mkdir -p "$conversation_folder"
+  fi
+
+  local default_model="${ASK_ME:-gemini/gemini-2.5-flash}"
+
+  aider --no-git \
+    --dry-run \
+    --model "$default_model" \
+    --input-history-file="$input_history_file" \
+    --chat-history-file="$chat_history_file" \
+    --llm-history-file="$llm_history_file" \
+    --restore-chat-history \
+    -m "'$*'"
 }
