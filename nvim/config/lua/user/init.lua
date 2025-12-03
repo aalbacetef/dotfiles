ba3cb5120b51b8a7dbdf2fbc1f29e40f8d939ced
Arturo Albacete <aalbacetef@gmail.com>
2025-12-03 08:20:10 +0100

    nvim: WIP new config after migrating to astronvim 5

---
+++ nvim/config/lua/user/init.lua
@@ -0,0 +1,33 @@
+vim.filetype.add({
+  extension = {
+    mdx = 'markdown.mdx'
+  }
+})
+
+vim.filetype.add({
+  extension = {
+    purs = 'purescript'
+  }
+})
+
+
+return {
+  -- load my config
+  { import = "user.plugins" },
+
+  -- ensure wrapping is enabled
+  { 
+    "AstroNvim/astrocore", 
+    opts = function(_, opts)
+      opts.options.opt.wrap = true
+      return opts
+    end 
+  },
+  {
+    "AstroNvim/astrolsp",
+    opts = function(_, opts)
+      opts.formatting.format_on_save.enabled = true 
+      return opts
+    end 
+  },
+}
